92876a31356d732226dac50d648dd455c0538650
Mateusz Lenczewski <mateusz.lenczewski@postadress.de>
2021-02-22 14:30:39 +0100

    Flask durch FastAPI ersetzt und Erweiterung der Endpoints

--- app.py
+++ app.py
@@ -1,32 +1,40 @@
 import robot
 import sys
-from flask import Flask, send_from_directory, jsonify, redirect, url_for, request
+from fastapi import FastAPI, Request
+from fastapi.responses import *
+from fastapi.staticfiles import StaticFiles
 
-app = Flask(__name__, static_url_path='')
 
 
-@app.route('/')
+APP_NAME= 'Robot-Webserver zum Starten von RobotTasks'
+app = FastAPI(title=APP_NAME)
+app.mount("/robotlog", StaticFiles(directory="robotlog"), name="robotlog")
+
+
+
+@app.get('/')
 def greetings():
     return 'Webserver zum Starten von RobotTasks'
 
 
-@app.route('/status/')
+@app.get('/status/')
 def server_status():
-    app.logger.info(f'Incoming status request...')
     status = {'Python Version': sys.version,
               'Platform': sys.platform,
-              'Arguments': sys.argv}
-    return jsonify(status)
+              'Arguments': sys.argv,
+              'Application': APP_NAME}
+    return status
 
 
-@app.route('/robotlog/<path:path>')
+@app.get('/robotlog/{path}')
 def browse_robot_log(path):
-    return send_from_directory('robotlog', path)
+    return RedirectResponse(path)
+    #return send_from_directory('robotlog', path)
 
 
-@app.route('/run/<path:task>')
+@app.get('/run/{task}')
 def do_some_work(task):
-    app.logger.info(f'Incoming request for task {task}')
+    #app.logger.info(f'Incoming request for task {task}')
     result: int = here_comes_work(task)
     if result == 0:
         result_page = 'PASS'
@@ -35,22 +43,29 @@ def do_some_work(task):
     else:
         result_page = f'FAIL: Errorcode {result}'
     result_page += '<p><a href="../robotlog/log.html">Go to log</a></p>'
-    return result_page
+    return Response(content=result_page, media_type="application/xml")
 
 
-@app.route('/run_and_show/<path:task>')
-def do_some_work_and_show_log(task):
-    here_comes_work(task)
-    return redirect(url_for('browse_robot_log', path='log.html'))
+@app.get('/run_and_show/{task}', response_class=HTMLResponse)
+def do_some_work_and_show_log(task: str, arguments: Request):
+    variables = [f'{k}:{v}' for k, v in arguments.query_params.items()]
+    here_comes_work(task, variables)
+    return RedirectResponse(f"/robotlog/{task}/log.html")
 
 
-def here_comes_work(task: str) -> int:
-    robot_variables: list = [f'{k}:{v}' for k, v in request.args.to_dict().items()]
+@app.get('/run_and_show_report/{task}', response_class=HTMLResponse)
+def do_some_work_and_show_report(task: str, arguments: Request):
+    variables = [f'{k}:{v}' for k, v in arguments.query_params.items()]
+    here_comes_work(task, variables)
+    return RedirectResponse(f"/robotlog/{task}/report.html")
+
+
+def here_comes_work(task: str, variables: list = None) -> int:
     result: int = robot.run(
         'tasks',
         task=task,
-        variable=robot_variables,
-        outputdir='robotlog',
+        outputdir=f'robotlog/{task}',
+        variables=variables,
         consolewidth=120
     )
     return result
