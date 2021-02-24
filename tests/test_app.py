9c9177858dc566e51f9778285b3ce4c1d4684762
Mateusz Lenczewski <mateusz.lenczewski@postadress.de>
2021-02-24 14:05:46 +0100

    Hinzufügen von Tests

---
+++ tests/test_app.py
@@ -0,0 +1,37 @@
+from fastapi.testclient import TestClient
+import unittest
+from app import app
+
+
+class EndpointTesttest_s(unittest.TestCase):
+
+    def test_is_service_available(self):
+        with TestClient(app) as client:
+            response = client.get("/status")
+        self.assertEqual(200, response.status_code)
+
+    def test_is_robottask_startable(self):
+        with TestClient(app) as client:
+            response = client.get("/run/nochEinTask")
+        self.assertEqual(200, response.status_code)
+
+    def test_is_robottask_available_with_logs(self):
+        with TestClient(app) as client:
+            response = client.get("/run_and_show/nochEinTask")
+        self.assertEqual(200, response.status_code)
+
+    def test_is_robottask_available_with_reports(self):
+        with TestClient(app) as client:
+            response = client.get("/run_and_show_report/nochEinTask")
+        self.assertEqual(200, response.status_code)
+
+    def test_is_robottask_available_with_logs_and_arguments(self):
+        with TestClient(app) as client:
+            response = client.get("/run_and_show/nochEinTask?art=tests&description=EreichbarkeitsTestMitLogs")
+        self.assertEqual(200, response.status_code)
+
+    def test_is_robottask_available_with_reports_and_arguments(self):
+        with TestClient(app) as client:
+            response = client.get("/run_and_show_report/nochEinTask?art=tests&description=FunktionsTestMitReports")
+        self.assertEqual(200, response.status_code)
+
