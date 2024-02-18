cb363cc04731c6a43a88ec478b041250fba312b2
Markus <5188411+Noordsestern@users.noreply.github.com>
2024-02-18 14:12:18 +0100

    Update setup.py
    drop official support for Python 3.7

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
 Framework :: FastAPI
 Framework :: Robot Framework
-Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
